13f51c3f6512a1d74b580e845f84525e99b43049
gab1280gh <gab1280gh@gmail.com>
2017-12-15 00:12:44 -0200

    Commit com pacotes, servlet e classe java de teste

---
+++ src/main/java/Modelo/Objeto.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Moon_
+ */
+public class Objeto {
+    
+    public int var;
+    
+}
